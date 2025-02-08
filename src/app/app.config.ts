484f8b59aade24178500efd7dd451d1400fa14fb
Amey Lokhande <amylokh@gmail.com>
2025-02-08 17:26:54 +0530

    Update Contact Component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,13 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { importProvidersFrom } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideAnimations()
+    provideAnimations(),
+    importProvidersFrom(FontAwesomeModule)
   ]
 };
