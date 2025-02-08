4e7f079a79e8b10baeb6c640e10919c1c678e2eb
Amey Lokhande <amylokh@gmail.com>
2025-02-08 23:34:34 +0530

    Update styling

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -2,12 +2,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { sectionAnimation, staggerFadeInUp } from '../../animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-experience',
   standalone: true, 
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   animations: [sectionAnimation, staggerFadeInUp],
   templateUrl: './experience.component.html'
 })
-export class ExperienceComponent {}
+export class ExperienceComponent {
+  calendarIcon = faCalendarAlt;
+}
